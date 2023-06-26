93624da31b2b81c50649c1834a5f2ca5e6ec2e95
bwithchewoo <loveablessing@gmail.com>
2023-06-26 11:43:06 -0400

    changed config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,12 +36,12 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+   config.action_cable.mount_path = nil
+   config.action_cable.url = 'wss://unogame.onrender.com/cable'
+   config.action_cable.allowed_request_origins = [ 'http://unogame.onrender.com', /http:\/\/unogame.onrender.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+   config.force_ssl = false
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
