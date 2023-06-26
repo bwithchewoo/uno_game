56425ba4662d137d3346b65a42595fdad08a547e
bwithchewoo <loveablessing@gmail.com>
2023-06-26 12:02:09 -0400

    actioncable url wss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
    config.action_cable.mount_path = nil
-   config.action_cable.url = 'ws://unogame.onrender.com/cable'
+   config.action_cable.url = 'wss://unogame.onrender.com/cable'
    config.action_cable.allowed_request_origins = [ 'http://unogame.onrender.com', /http:\/\/unogame.onrender.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
